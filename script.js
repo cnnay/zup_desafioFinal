ee8b6e28a903036f66f6c586eb5d64647f42b659
cnnay <carlaalraccn2018@gmail.com>
2021-06-14 22:11:33 -0300

    desenvolvimento da parte lógica do retorno da API, estilização do projeto

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-fetch('https://rickandmortyapi.com/api/character', {
-    method: 'GET',
-    Headers: {
-        Accept: 'application/json',
-        "Content-type": 'application/json'
-    } 
-})
-.then(response => response.json())
-.then(function(json){
+function gerarNumeroAleatorio(max) {
+    return Math.floor(Math.random() * max);
+    //função que retorna numero aleatório
+}
 
-    let container = document.querySelector('.container');
-    console.log(json)
+let var1 = gerarNumeroAleatorio(671);
+let var2 = gerarNumeroAleatorio(671);
+let var3 = gerarNumeroAleatorio(671);
+let var4 = gerarNumeroAleatorio(671);
 
-    json.results.map(function(results){
-        
+// declaração de variáveis para os numeros aleatórios
 
-        container.innerHTML +=`
-        <div> 
-            <img src=`+ results.image + `> 
-            <p> `+results.name + `</p>
-            <p> `+results.species + `</p>
-            <p> `+results.status + `</p>
-        </div>
-        `;
+let personagens = [var1, var2, var3, var4]
+// Array de numeros aleatórios
 
-
+personagens.forEach(function(item) {
+    fetch('https://rickandmortyapi.com/api/character/' + item, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        } // chamada na Api
     })
+    .then(response => response.json())
+    .then(function(result) {
+        let container = document.querySelector('#container');
 
-    
+        container.innerHTML += `<div>
+        <img src=` + result.image + `>  
+        <p> `+ 'Nome:  ' + result.name + `</p>
+        <p> ` + 'Especie:  ' + result.species + `</p>
+        <br>
+        </div>`;
 
-})
+        //resposta da Api manipulando uma tag HTML
+    })    
+});
